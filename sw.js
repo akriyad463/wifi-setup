7a423ccd80c8ede6796a1bc828e56d71d35424ba
Ak RIYAD <akriyad33099@gmail.com>
2026-04-12 08:13:14 +0400

    Refactor service worker with manifest details
    Updated service worker to include a manifest configuration and removed caching logic.

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const cacheName = 'wifi-setup-v9'; 
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
+{
+  "name": "WIFI-SETUP Mobile Gateway",
+  "short_name": "WiFiSetup",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#000814",
+  "theme_color": "#00d4ff",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
